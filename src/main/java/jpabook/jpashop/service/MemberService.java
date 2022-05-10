536f660de0dd9946afd46e1b26bded365d86c7b6
daewancha <dwcha0524@gmail.com>
2022-05-10 22:38:46 +0900

    회원 수정 API

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -37,4 +37,13 @@ public class MemberService {
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
+
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
